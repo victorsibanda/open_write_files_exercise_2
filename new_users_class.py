76ae95f7153905de464e872204db5dfffa6c5e9f
Victor Sibanda <victormsibanda1@gmail.com>
2020-03-11 22:31:50 +0000

    task complete

---
+++ new_users_class.py
@@ -0,0 +1,7 @@
+class New_users():
+    def __init__(self,name):
+        self.name = name
+
+    def user_info(self):
+        with open(self.name + '.txt', 'a+') as f:
+            f.write(self.name)
---
+++ run_tasks.py
@@ -0,0 +1,25 @@
+from new_users_class import *
+
+user_list = []
+# open_and_print('names.txt')
+
+try:
+    with open('names.txt', 'r') as file:
+        lines_list = file.readlines()
+        for line in lines_list:
+            print(line.rstrip('\n').title())
+            user = New_users(line.rstrip('\n'))
+            user_list.append(user)
+
+except FileNotFoundError as err:
+    print('check your file')
+    print(err)
+finally:
+    print('-----Text file Read----')
+
+print(user_list)
+
+
+
+
+# create_object('names.txt')
